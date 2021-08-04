3f87450b2e3215b7519f2804676926681ebc3345
DESKTOP-TAE0S6J\刘龙 <3356466085@qq.com>
2021-08-04 10:35:42 +0800

    logs

---
+++ ExpressCode.Common/Log/LogModel/AopNlog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressCode.Common
+{
+    /// <summary>
+    /// Aop日志表
+    /// </summary>
+    public class AopNlog
+    {
+        public string MethodName { get; set; }      //方法名
+        public string ReturnType { get; set; }      //返回类型
+        public string ReturnUrl { get; set; }       //返回路径
+        public string ReturnResult { get; set; }    //返回结果 
+        public DateTime ReturnTime { get; set; }    //返回时间
+    }
+}
